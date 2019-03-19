e6f572cda45524b133b1e13e69bcab14b7795199
wb-wp504288 <pei.wang1@bangdao-tech.com>
2019-03-19 20:53:22 +0800

    代码提交

---
+++ Jsonob.js
@@ -0,0 +1,85 @@
+const OP = Object.prototype;
+
+/*
+ *  需要重写的数组方法 OAR 是 overrideArrayMethod 的缩写
+ */
+const OAM = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+
+class Jsonob{
+    constructor(obj, callback){
+        if(OP.toString.call(obj) !== '[object Object]'){
+            console.error('This parameter must be an object：' + obj);
+        }
+        this.$callback = callback;
+        this.observe(obj);
+    }
+
+    observe(obj,path){
+        // 如果发现 监测的对象是数组的话就要调用 overrideArrayProto 方法
+        if(OP.toString.call(obj) === '[object Array]'){
+            this.overrideArrayProto(obj,path);
+        }
+        Object.keys(obj).forEach(function(key, index, keyArray){
+            var oldVal = obj[key];
+            var pathArray = path && path.slice(0);
+            if(pathArray){
+                pathArray.push(key);
+            }else{
+                pathArray = [key];
+            }
+            Object.defineProperty(obj, key, {
+                get: function(){
+                    return oldVal;
+                },
+                set: (function(newVal){
+                    if(oldVal != newVal){
+                        this.$callback(newVal,oldVal,pathArray);
+                        oldVal = newVal;
+                    }
+                }).bind(this)
+            });
+
+            if(OP.toString.call(obj[key]) === '[object Object]' || OP.toString.call(obj[key]) === '[object Array]'){
+                this.observe(obj[key],pathArray);
+            }
+
+        }, this);
+
+    }
+
+    // array监听数据变化
+    overrideArrayProto(array,path){
+         // 保存原始 Array 原型
+         var originalProto = Array.prototype,
+         // 通过 Object.create 方法创建一个对象，该对象的原型就是Array.prototype
+         overrideProto = Object.create(Array.prototype),
+         self = this,
+         result;
+         // 遍历要重写的数组方法
+         Object.keys(OAM).forEach(function(key, index, array){
+             var method = OAM[index],
+             oldArray=[];
+             // 使用 Object.defineProperty 给 overrideProto 添加属性，属性的名称是对应的数组函数名，值是函数
+             Object.defineProperty(overrideProto, method, {
+                 value:function(){
+                     oldArray = this.slice(0);
+                     console.log(`oldArray:${JSON.stringify(oldArray)}`);
+                     var arg = [].slice.apply(arguments);
+                     // 调用原始 原型 的数组方法
+                     result = originalProto[method].apply(this,arg);
+                     //对新的数组进行监测
+                     self.observe(this,path);
+                     //执行回调
+                     self.$callback(this,oldArray,path);
+
+                     return result;
+                 },
+                 writable:true,// 是否可写
+                 enumerable:false,// 是否能够通过for in 枚举
+                 configurable:true // 是否可使用 delete删除
+             });
+         },this);
+         // 最后 让该数组实例的 __proto__ 属性指向 假的原型 overrideProto
+         array.__proto__ = overrideProto;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mvvmModel1
 MVVM 双向绑定的原理（object对象，普通对象 及 Array数组的处理，学习vue）
+此demo实现了数据的get/set监听
+普通对象的处理
+数组对象的监听变化
+监听变化数据的路径
